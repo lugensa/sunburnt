f1c68e02357ed5e1a419570ba007f696cbd44762
David Beitey <david@davidjb.com>
2012-07-03 15:28:56 +1000

    Switching to Distribute

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
-import distutils.core, os, re
+import os, re
+
+try:
+    from setuptools import setup
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup
 
 version_number_re = "\s*__version__\s*=\s*((\"([^\"]|\\\\\")*\"|'([^']|\\\\')*'))"
 version_file = os.path.join(os.path.dirname(__file__), 'sunburnt', '__init__.py')
 version_number = re.search(version_number_re, open(version_file).read()).groups()[0][1:-1]
 
-distutils.core.setup(
+setup(
     name='sunburnt',
     version=version_number,
     description='Python interface to Solr',
@@ -14,6 +21,11 @@ distutils.core.setup(
     author_email='toby@timetric.com',
     packages=['sunburnt'],
     requires=['httplib2', 'lxml', 'pytz'],
+    install_requires=[
+        'httplib2',
+        'lxml',
+        'pytz'
+    ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
