e71ad61cfcad28cfc7d15f2e5661f309efa8741a
Josip Delic <delijati@googlemail.com>
2014-03-12 16:18:36 +0100

    removed duplicated code

--- sunburnt/tests/test_dates.py
+++ sunburnt/tests/test_dates.py
@@ -13,38 +13,38 @@ not_utc = pytz.timezone('Etc/GMT-3')
 
 samples_from_pydatetimes = {
     "2009-07-23T03:24:34.000376Z":
-    [datetime.datetime(2009, 07, 23, 3, 24, 34, 376),
-     datetime.datetime(2009, 07, 23, 3, 24, 34, 376, pytz.utc)],
+    [datetime.datetime(2009, 7, 23, 3, 24, 34, 376),
+     datetime.datetime(2009, 7, 23, 3, 24, 34, 376, pytz.utc)],
     "2009-07-23T00:24:34.000376Z":
-    [not_utc.localize(datetime.datetime(2009, 07, 23, 3, 24, 34, 376)),
-     datetime.datetime(2009, 07, 23, 0, 24, 34, 376, pytz.utc)],
+    [not_utc.localize(datetime.datetime(2009, 7, 23, 3, 24, 34, 376)),
+     datetime.datetime(2009, 7, 23, 0, 24, 34, 376, pytz.utc)],
     "2009-07-23T03:24:34Z":
-    [datetime.datetime(2009, 07, 23, 3, 24, 34),
-     datetime.datetime(2009, 07, 23, 3, 24, 34, tzinfo=pytz.utc)],
+    [datetime.datetime(2009, 7, 23, 3, 24, 34),
+     datetime.datetime(2009, 7, 23, 3, 24, 34, tzinfo=pytz.utc)],
     "2009-07-23T00:24:34Z":
-    [not_utc.localize(datetime.datetime(2009, 07, 23, 3, 24, 34)),
-     datetime.datetime(2009, 07, 23, 0, 24, 34, tzinfo=pytz.utc)]
+    [not_utc.localize(datetime.datetime(2009, 7, 23, 3, 24, 34)),
+     datetime.datetime(2009, 7, 23, 0, 24, 34, tzinfo=pytz.utc)]
 }
 
 if HAS_MX_DATETIME:
     samples_from_mxdatetimes = {
         "2009-07-23T03:24:34.000376Z":
-        [mx.DateTime.DateTime(2009, 07, 23, 3, 24, 34.000376),
-         datetime.datetime(2009, 07, 23, 3, 24, 34, 376, pytz.utc)],
+        [mx.DateTime.DateTime(2009, 7, 23, 3, 24, 34.000376),
+         datetime.datetime(2009, 7, 23, 3, 24, 34, 376, pytz.utc)],
         "2009-07-23T03:24:34Z":
-        [mx.DateTime.DateTime(2009, 07, 23, 3, 24, 34),
-         datetime.datetime(2009, 07, 23, 3, 24, 34, tzinfo=pytz.utc)],
+        [mx.DateTime.DateTime(2009, 7, 23, 3, 24, 34),
+         datetime.datetime(2009, 7, 23, 3, 24, 34, tzinfo=pytz.utc)],
     }
 
 
 samples_from_strings = {
     # These will not have been serialized by us, but we should deal with them
     "2009-07-23T03:24:34Z":
-    datetime.datetime(2009, 07, 23, 3, 24, 34, tzinfo=pytz.utc),
+    datetime.datetime(2009, 7, 23, 3, 24, 34, tzinfo=pytz.utc),
     "2009-07-23T03:24:34.1Z":
-    datetime.datetime(2009, 07, 23, 3, 24, 34, 100000, pytz.utc),
+    datetime.datetime(2009, 7, 23, 3, 24, 34, 100000, pytz.utc),
     "2009-07-23T03:24:34.123Z":
-    datetime.datetime(2009, 07, 23, 3, 24, 34, 122999, pytz.utc)
+    datetime.datetime(2009, 7, 23, 3, 24, 34, 122999, pytz.utc)
 }
 
 
@@ -55,8 +55,8 @@ def check_solr_date_from_date(s, date, canonical_date):
 
 
 def check_solr_date_from_string(s, date):
-    assert solr_date(s)._dt_obj == date, "Unequal representations of %r: %r" % (
-        solr_date(s)._dt_obj, date)
+    assert solr_date(s)._dt_obj == date, "Unequal representations of %r: %r and %r" % (
+        solr_date(s)._dt_obj, date, s)
 
 
 def test_solr_date_from_pydatetimes():
